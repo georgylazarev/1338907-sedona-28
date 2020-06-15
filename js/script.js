842e40035ea23b5d60bff30442e31f2682a354b4
Georgy Lazarev <geolaz@gmail.com>
2020-06-15 11:46:15 +0300

    проверка полей формы + префикс + мин + скрипт компресс

--- js/script.js
+++ js/script.js
@@ -1,7 +1,52 @@
-let searchLink = document.querySelector(".hotel-search-link");
-let pop = document.querySelector(".hotel-popup");
+var searchLink = document.querySelector(".hotel-search-link");
+var pop = document.querySelector(".hotel-popup");
+
+var searchForm = document.querySelector(".search-form");
+var startDate = document.querySelector("#start-date");
+var endDate = document.querySelector("#end-date");
+var adultCount = document.querySelector("#adult-count");
+var childrenCount = document.querySelector("#children-count");
 
 searchLink.addEventListener("click", function(evt) {
   evt.preventDefault();
   pop.classList.toggle("hotel-popup-show");
+
+  if (pop.classList.contains("error-message")) {
+    pop.classList.remove("error-message");
+  }
+
+  try {
+    storageAdult = localStorage.getItem("adultCount");
+    storageChildren = localStorage.getItem("childrenCount");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+  if(storageAdult || storageChildren) {
+    adultCount.value = storageAdult;
+    childrenCount.value = storageChildren;
+  }
+});
+
+searchForm.addEventListener("submit", function(evt) {
+  if (!startDate.value || !endDate.value || !adultCount.value || !childrenCount.value) {
+    evt.preventDefault();
+    pop.classList.add("error-message");
+  }
+  else {
+    localStorage.setItem("adultCount", adultCount.value);
+    localStorage.setItem("childrenCount", childrenCount.value);
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (pop.classList.contains("hotel-popup-show")) {
+      evt.preventDefault();
+      pop.classList.remove("hotel-popup-show");
+    }
+    if (pop.classList.contains("error-message")) {
+      pop.classList.remove("error-message");
+    }
+  }
 });
